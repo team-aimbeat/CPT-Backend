a6c0de35135bd84768f8c62de3f3cc9df044ec29
poojadhuri123 <poojadhurijagannath@gmail.com>
2026-02-11 11:20:43 +0530

    Comment out exercise video menu items for future reference

--- resources/views/partials/dashboard/vertical-nav.blade.php
+++ resources/views/partials/dashboard/vertical-nav.blade.php
@@ -128,6 +128,7 @@
                 ->prepend('<i class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor"><g><circle cx="12" cy="12" r="8" fill="currentColor"></circle></g></svg></i>')
                 ->link->attr(['class' => activeRoute(route('exercise.create')) || request()->is('exercise/*/edit') ? 'nav-link active' : 'nav-link']);
 
+            /*
             $menu->exercise->add('<span class="item-name">Video List</span>', ['route' => 'exercise.video.list'])
                 ->data('permission', 'exercise-list')
                 ->prepend('<i class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor"><g><circle cx="12" cy="12" r="8" fill="currentColor"></circle></g></svg></i>')
@@ -137,6 +138,7 @@
                 ->data('permission', [ 'exercise-add', 'exercise-edit'])
                 ->prepend('<i class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor"><g><circle cx="12" cy="12" r="8" fill="currentColor"></circle></g></svg></i>')
                 ->link->attr(['class' => activeRoute(route('exercise.video.create')) ? 'nav-link active' : 'nav-link']);
+            */
         
         $menu->add('<span class="item-name">'.__('message.workout').'</span>', ['class' => ''])
             ->prepend('<i class="icon">
